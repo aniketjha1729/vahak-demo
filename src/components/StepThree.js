c2ca0fa989b832953f4a3c977c1a38d063ddd9f9
aniketjha1729 <kumaraniket1729@gmail.com>
2022-02-13 17:27:40 +0530

    step two integrated

--- src/components/StepThree.js
+++ src/components/StepThree.js
@@ -2,15 +2,25 @@ import React from "react";
 import { Formik, Form, Field } from "formik";
 import Header from "./Header";
 
-const StepThree = () => {
+const StepThree = (props) => {
+  const handleSubmit = (values) => {
+    props.next(values);
+  };
   return (
     <>
       <Header heading="Place your Bid (3/4 step)" />
-      <Formik>
-        {() => (
+      <Formik initialValues={props.data} onSubmit={handleSubmit}>
+        {({ values }) => (
           <Form>
-            <p>bidAmount</p>
-            <Field name="bidAmount" />
+            <p>Mobile</p>
+            <Field name="mobile" />
+            <p>name</p>
+            <Field name="name" />
+            <p>remarks</p>
+            <Field name="remarks" />
+            <button type="button" onClick={() => props.prev(values)}>
+              Back
+            </button>
             <button type="submit">Next</button>
           </Form>
         )}
--- src/components/StepTwo.js
+++ src/components/StepTwo.js
@@ -1,33 +1,74 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import TextField from "./formUi/TextField";
 import Header from "./Header";
+import * as Yup from "yup";
+import "./steps.css";
+
+const stepTwoValidate = Yup.object({
+  bidAmount: Yup.number().required().label("Enter your bid amount."),
+});
 
 const StepTwo = (props) => {
   const handleSubmit = (values) => {
     props.next(values);
   };
   return (
-    <>
+    <div>
       <Header heading="Place your Bid (2/4 step)" />
-      <Formik initialValues={props.data} onSubmit={handleSubmit}>
-        {({ values }) => (
-          <Form>
-            <div className="form">
-              <p>Mobile</p>
-              <Field name="mobile" />
-              <p>name</p>
-              <Field name="name" />
-              <p>remarks</p>
-              <Field name="remarks" />
-              <button type="button" onClick={() => props.prev(values)}>
-                Back
-              </button>
-              <button type="submit">Next</button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </>
+      <div className="formConatiner">
+        <Formik
+          validationSchema={stepTwoValidate}
+          initialValues={props.data}
+          onSubmit={handleSubmit}
+        >
+          {({ values }) => (
+            <Form>
+              <div className="form">
+                <div className="stepTwo-prevDataContainer">
+                  <div className="stepTwo-prevData">
+                    <h5>JOURNEY DETAILS</h5>
+                    <p>
+                      {props.data.sourceDestination} ---
+                      {props.data.destination}
+                    </p>
+                    <p>
+                      {props.data.numberOfTravellers} Persons,
+                      {props.data.carType}
+                    </p>
+                  </div>
+                  <div className="prevButton">
+                    <button type="button" onClick={() => props.prev(values)}>
+                      Back
+                    </button>
+                  </div>
+                </div>
+                <div className="dividerContainer">
+                  <Divider />
+                </div>
+                <div className="bidAmountContainer">
+                  <div className="rupeeSymbol">&#8377;</div>
+                  <div>
+                    <TextField name="bidAmount" />
+                  </div>
+                </div>
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Next
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 };
 
